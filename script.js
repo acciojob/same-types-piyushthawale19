784c09519226bb5ec179a669e2dff41fd99cc77c
PIYUSH ARUN THAWLE <151814546+piyushthawale19@users.noreply.github.com>
2026-05-23 11:43:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ if(Number.isNaN(value1)&&Number.isNaN(value2)){
 	if(Number.isNaN(value1)||Number.isNaN(value2)){
 	return false;
 }
+	
 	return typeof value1 === typeof value2;
 }
 
 // do not change the code below
-let value1 = JSON.parse("Enter Start of the Range.");
-let value2 = JSON.parse("Enter End Of the Range.");
+let value1 = JSON.parse(prompt("Enter Start of the Range."));
+let value2 = JSON.parse(prompt("Enter Start of the Range."));
 alert(isSameType(value1, value2));

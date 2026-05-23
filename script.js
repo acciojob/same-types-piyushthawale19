756bc38c4afa8e9b0e56eafbda44fc72a23412b4
PIYUSH ARUN THAWLE <151814546+piyushthawale19@users.noreply.github.com>
2026-05-23 11:53:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 function isSameType(value1, value2) {
-  
-if(Number.isNaN(value1)&&Number.isNaN(value2)){
-	return true;
-}
-	if(Number.isNaN(value1)||Number.isNaN(value2)){
-	return false;
-}
-	
-	return typeof value1 === typeof value2;
+
+    if (Number.isNaN(value1) && Number.isNaN(value2)) {
+        return true;
+    }
+
+    if (Number.isNaN(value1) || Number.isNaN(value2)) {
+        return false;
+    }
+
+       return typeof value1 === typeof value2;
+
 }
 
-// do not change the code below
-let value1 = JSON.parse(prompt("Enter Start of the Range."));
-let value2 = JSON.parse(prompt("Enter Start of the Range."));
+
+// do not change below
+let value1 = prompt("Enter Start of the Range.");
+let value2 = prompt("Enter End of the Range.");
+
+value1 = isNaN(value1) ? value1:Number(value1);
+value2 = isNaN(value2) ? value2:Number(value2);
+
 alert(isSameType(value1, value2));
